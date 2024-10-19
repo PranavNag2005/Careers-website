78eddc71d4b969b0152da9566a989e5bd0c4f5e0
PranavNag2005 <153430540+PranavNag2005@users.noreply.github.com>
2024-10-19 14:22:17 +0000

    added jobs to the page in the form of list  and styled the page using of bootstrap css

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+Jobs = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs.10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data scientist',
+    'location': 'Hyderabad, India',
+    'salary': 'Rs.12,00,000'
+}, {
+    'id': 3,
+    'title': 'Front end Developer',
+    'location': 'Remote',
+    'salary': 'Rs.5,00,000'
+}, {
+    'id': 4,
+    'title': 'Backend Developer',
+    'location': 'california, U.S.A',
+}]
+
 
 @app.route("/")
-
 def hello_world():
-  return render_template('home.html')
-if __name__ =='__main__':
-  app.run(host='0.0.0.0',debug=True)
+  return render_template('home.html', jobs=Jobs)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
